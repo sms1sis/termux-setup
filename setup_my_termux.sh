75817ce9dbaa463eb88b3028f973410957fc692c
sms1sis <sadimsajid2004@gmail.com>
2025-10-20 00:42:10 +0600

    Fix: Remove redundant plugin sourcing in .zshrc

--- setup_my_termux.sh
+++ setup_my_termux.sh
@@ -40,8 +40,6 @@ export ZSH="$HOME/.oh-my-zsh"
 plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
 
 source $ZSH/oh-my-zsh.sh
-source $ZSH/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-source $ZSH/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 alias ls="lsd"
 
 
